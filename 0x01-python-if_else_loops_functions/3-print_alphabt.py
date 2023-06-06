246e9cd8b4cee19793207203f1771733350a01b5
exclud <shammahnjomo12@gmail.com>
2023-06-06 21:52:41 +0300

    Fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(i)
-              for i in range(ord('a'), ord('z')+1)
-              if chr(i) not in {'q', 'e'}))
+print(''.join(chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in {'q', 'e'}))
