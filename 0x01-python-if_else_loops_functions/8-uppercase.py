092ff34ee5a4a279c89a1759d3f97df4a2b4345c
exclud <shammahnjomo12@gmail.com>
2023-06-06 21:09:34 +0300

    prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:  # If the character is a lowercase letter
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
