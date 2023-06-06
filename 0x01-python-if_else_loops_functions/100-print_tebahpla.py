2a0ed9aaf889521e8af4d8b5a30ad838461542fb
exclud <shammahnjomo12@gmail.com>
2023-06-06 22:10:20 +0300

    Pycode Style Validation

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join("{}{}".format(chr(i), chr(i).upper() if i % 2 else "") for i in range(122, 96, -1)))
+print("".join("{}{}".format(chr(i), chr(i).upper()
+                            if i % 2 else "")
+              for i in range(122, 96, -1)))
