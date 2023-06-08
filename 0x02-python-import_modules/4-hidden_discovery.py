976671640f7fa22a4719bc5acc9799bf937fc3c1
exclud <shammahnjomo12@gmail.com>
2023-06-08 22:02:06 +0300

    Pycodestyle validation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,8 +6,8 @@ def main():
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
-            print(name)         
+            print(name)
+
 
 if __name__ == "__main__":
     main()
-        
