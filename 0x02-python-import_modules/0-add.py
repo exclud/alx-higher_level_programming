db0d724227f9a4348dfb4a9447addee54545631e
exclud <shammahnjomo12@gmail.com>
2023-06-08 21:39:29 +0300

    Addition Fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,8 +5,8 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}\n".format(a, b, result))
+# result = add(a, b)
+    print("{} + {} = {}\n".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
