46249277a76da410ab6be2e08ce6b30afbbc53fe
exclud <shammahnjomo12@gmail.com>
2023-06-08 22:00:04 +0300

    Traceback error fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,10 +4,9 @@ import hidden_4
 
 def main():
     names = dir(hidden_4)
-    for name in sorted (names):
-        if not name.startswith('--'):
-            print(name)
-            
+    for name in names:
+        if name[:2] != "__":
+            print(name)         
 
 if __name__ == "__main__":
     main()
