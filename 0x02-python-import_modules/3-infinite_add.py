9b09f4f13be23dae886e2f9c864b5f64611fe1b3
exclud <shammahnjomo12@gmail.com>
2023-06-08 21:32:22 +0300

    Pycodesytle Validation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
     args_as_ints = [int(arg) for arg in sys.argv[1:]]
-    
     print(sum(args_as_ints))
-    
+
+
 if __name__ == "__main__":
     main()
-   
