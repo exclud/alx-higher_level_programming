2fd85d3b6207bc53e93b73eb14e50473a850baab
exclud <shammahnjomo12@gmail.com>
2023-06-08 21:35:42 +0300

    Calculator Fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,17 +5,12 @@ from calculator_1 import add, sub, mul, div
 def main():
     a = 10
     b = 5
-
-    add_result = add(a, b)
-    sub_result = sub(a, b)
-    mult_result = mul(a, b)
-    div_result = div(a, b)
-
-
-    print(f'{a} + {b} = {add_result}')
-    print(f'{a} - {b} = {sub_result}')
-    print(f'{a} * {b} = {mult_result}')
-    print(f'{a} / {b} = {div_result}')
+    
+    
+    print(f'{a} + {b} = {add(a, b)}')
+    print(f'{a} - {b} = {sub(a, b)}')
+    print(f'{a} * {b} = {mul(a, b)}')
+    print(f'{a} / {b} = {div(a, b)}')
 
 
 if __name__ == "__main__":
