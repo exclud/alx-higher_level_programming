581e9189fbc3bdc7bbb14688a1a21f05b70f67d1
exclud <shammahnjomo12@gmail.com>
2023-06-08 22:25:14 +0300

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    argc = len(sys.argv) - 1
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    result = operators[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Higher Level Programming in Python
 Folders :
 1. 0x00. Python- Hello World.
 2. 0x01-python-if_else_loops_functions
+3. 0x02. Python - import & modules
