b54f001ea44893e46f7ad1c8c79bc2761a2e3242
exclud <shammahnjomo12@gmail.com>
2023-06-14 23:55:18 +0300

    Funtion that returns a new dictionary with values * 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for key in a_dictionary:
+        new_dictionary[key] = a_dictionary[key] * 2
+    return new_dictionary
+
+# def multiply_by_2(a_dictionary):
+#     return {k : v * 2 for k, v in a_dictionary.items()}
