4c73c009a8d69dc7f0fc8d3a2394a5c6723b0b58
exclud <shammahnjomo12@gmail.com>
2023-07-04 19:53:17 +0300

    Wrote sucessfull tests for "say my name"

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,8 +18,10 @@ def matrix_divided(matrix, div):
     Returns:
         list of lists: The new matrix with elements divided by div.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               for row in matrix):
+        raise TypeError
+    ("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print the full name in the format 
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to an empty string.
+
+    Raises:
+        TypeError: If first name is not a string
+        TypeError: If last name is not a string
+
+    Returns:
+        None
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
