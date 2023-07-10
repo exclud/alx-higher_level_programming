2aac284aca97313b623b88277e4de270143fc4a4
exclud <shammahnjomo12@gmail.com>
2023-07-10 16:10:00 +0300

    A function that returns avail attr and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def lookup(obj):
+    """A function that returns the list of avail attributes and methods
+
+    Args:
+        obj : The object to check
+
+    Returns:
+        list: List containing attributes and methods
+    """
+    return dir(obj)
