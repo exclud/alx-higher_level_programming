3033f659a01b5bddf0aaa24661891f1ea6345a35
exclud <shammahnjomo12@gmail.com>
2023-07-10 17:40:32 +0300

    Square Fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,14 +3,31 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square Representation"""
+    """
+    Class representing a square, which is a special case of a rectangle.
+    """
 
     def __init__(self, size):
-        """Initialize new square.
+        """
+        Initialize a square with a given size.
 
         Args:
-            size (int): size of the new square.
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is not greater than 0.
+
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
+        return self._Rectangle__width * self._Rectangle__height
