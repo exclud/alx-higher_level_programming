aa5ebefc5aed15a585030cc7ddbbe2db870d51f1
exclud <shammahnjomo12@gmail.com>
2023-07-10 17:17:58 +0300

    Class which inherits from a class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""An Empty Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class for geometry calculations
+    """
+    def area(self):
+        """Area Calculations
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates Value
+
+        Args:
+            name (string): name of value
+            value (int): value to validate
+
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less or equal to 0
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Class Rectangle that inherits from BaseGeometry Class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle width and height
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
