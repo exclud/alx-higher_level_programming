1842d8bcfd78f8cd527442658460938e8e23554a
exclud <shammahnjomo12@gmail.com>
2023-07-10 16:49:42 +0300

    An empty class BaseGeometry

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,14 +4,14 @@
 
 def inherits_from(obj, a_class):
     """
-    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+    Check if object is an instance of class inherited from the specified class.
 
     Args:
         obj: The object to check.
         a_class: The specified class to compare against.
 
     Returns:
-        bool: True if the object is an instance of a class that inherited (directly or indirectly) from the specified
+        True:if object is nstance of a class that inherited from the specified
         class; False otherwise.
 
     """
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""An Empty Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An instance of an empty class
+    """
+    pass
