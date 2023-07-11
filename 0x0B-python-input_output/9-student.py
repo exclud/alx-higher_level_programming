633c56c1e1e043b26571d2bad944f9e7ea148564
exclud <shammahnjomo12@gmail.com>
2023-07-11 17:00:03 +0300

    Class Student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Student Representation"""
+    
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student
+
+        Args:
+            first_name (str): First Name
+            last_name (str): Last Name
+            age (int): Age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def to_json(self, attrs=None):
+        """Get dictionary rep of the student
+
+        Args:
+            attrs (, optional): attributes to represent. Defaults to None.
+
+
+        """
+        if (type(attrs)) == list and all(type(ele) == str for ele in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+       
