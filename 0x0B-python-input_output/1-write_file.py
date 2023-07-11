582efd004aaefbcbe84739a728475231266103f9
exclud <shammahnjomo12@gmail.com>
2023-07-11 15:54:15 +0300

    String to write a string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+"""A function that writes a string text to a file"""
+def write_file(filename="", text=""):
+    """Function to write string text to a file
+
+    Args:
+        filename (str, optional): Name of the filename. Defaults to "".
+        text (str, optional): Text to be written in the file. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
+    with open (filename, 'w', encoding='utf-8') as file:
+        num_chars_written = file.write(text)
+    return num_chars_written
