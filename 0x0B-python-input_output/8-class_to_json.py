0df8ad5c7bd19eadb2a961f0ff717b66df2c17bd
exclud <shammahnjomo12@gmail.com>
2023-07-11 16:50:16 +0300

    Return dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""""Return dictionary description"""
+
+
+def class_to_json(obj):
+    """Return dict description
+
+    Args:
+        obj (obj): object to serialize
+
+    Returns:
+        json_data: dictionary that can be used to serialize the obj to JSON
+    """
+    json_data = {}
+
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_data[key] = value
+        else:
+            json_data[key] = class_to_json(value)
+
+    return json_data
