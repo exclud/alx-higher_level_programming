f0389f15e142cbfc4f9b335655ed689f08b1e422
exclud <shammahnjomo12@gmail.com>
2023-07-11 16:27:32 +0300

    A function that creates an Objec from JSON file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""A function to write an object to a text file using JSON rep"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an Object to a text file using JSON rep
+
+    Args:
+        my_obj (): Object to serialize
+        filename (): Name of file to be written
+    """
+    with open(filename, 'w') as o:
+        json.dump(my_obj, o)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Function to create an Object from JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from JSON file
+
+    Args:
+        my_obj (): The Object
+        filename (): Name of file to read
+
+    Returns:
+        data: object representation
+    """
+    with open(filename, 'r') as o:
+        data = json.load(o)
+    return data
