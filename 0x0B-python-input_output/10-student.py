7a2c56ce5b7e4e409a3aac0a7aa642d958d3fb21
exclud <shammahnjomo12@gmail.com>
2023-07-11 17:10:52 +0300

    Class Modification

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Student Representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student
+
+        Args:
+            first_name (str): First Name
+            last_name (str): Last Name
+            age (int): Age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get dictionary rep of the student
+
+
+        """
+        return self.__dict__
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     """Student Representation"""
-    
+
     def __init__(self, first_name, last_name, age):
         """Initialize Student
 
@@ -16,7 +16,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         """Get dictionary rep of the student
 
@@ -28,4 +28,3 @@ class Student:
         if (type(attrs)) == list and all(type(ele) == str for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-       
