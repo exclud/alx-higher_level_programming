43b5d5663cba13f67b99921d86cac3317c67ceea
exclud <shammahnjomo12@gmail.com>
2023-07-11 16:39:53 +0300

    Add all args to a Python list and save to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Add all args to a python list and save them to fiel"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """Add all args to a python list
+    """
+    list_of_args = []
+
+    for arg in sys.argv[1]:
+        list_of_args.append(arg)
+
+    json_data = {"args": list_of_args}
+
+    save_to_json_file(json_data, "add_item.json")
+
+
+if __name__ == "__main__":
+    main()
