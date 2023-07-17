bfb7e812271e599d98532f5c9955cbee3b327949
exclud <shammahnjomo12@gmail.com>
2023-07-17 21:57:15 +0300

    Save class instances to a JSON file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-      
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Converts list of dictionaries to JSON
@@ -38,3 +38,18 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Saves the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): List of instances inheriting from Base class
+        """
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(json_list)
+        with open(filename, "w") as file:
+            file.write(json_string)
