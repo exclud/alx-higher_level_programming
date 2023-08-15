a8ad89971a0012f63b5bb4dadce785bba9f29f16
exclud <shammahnjomo12@gmail.com>
2023-08-15 12:26:30 +0300

    Syntax Error Fix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Script to create table 'first_table' om the current database
 
-CREATE TABLE IF NOT EXISTS first_table(id INT name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'first_table' 'id' INT 'name' VARCHAR(256));
