08e75c8dddd92bb07c7f1ab65e497e94312992dc
exclud <shammahnjomo12@gmail.com>
2023-08-16 13:57:48 +0300

    script that creates the table force_name on your MySQL server.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
--- A script to list all privillages of the MySQL users.
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- This script lists all privileges of MySQL users user_0d_1 and user_0d_2
+
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
