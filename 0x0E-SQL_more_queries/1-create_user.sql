24b15189dc357602bdff10391f3db447ca8e47f3
exclud <shammahnjomo12@gmail.com>
2023-08-16 13:02:05 +0300

    a script that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,10 @@
 -- Script to create MySQL server user user_0d_1.
 -- This creates the user with all privillages
-CREATE USER IF NOT EXISTS 'user_0d_1.'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- This grants the user all privillages
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
