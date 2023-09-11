ef6049b7323e9762fb88afc5227221459fb61f99
exclud <shammahnjomo12@gmail.com>
2023-09-11 23:22:03 +0300

    script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 !== undefined && arg2 !== undefined) {
+  console.log(`${arg1} is ${arg2}`);
+} else {
+  console.log('Usage: ./script.js <argument1> <argument2>');
+}
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Get the first argument passed to the script
+const arg1 = process.argv[2];
+
+// Use parseInt to attempt conversion to an integer
+const parsedValue = parseInt(arg1);
+
+// Check if parsedValue is a valid integer
+if (!isNaN(parsedValue)) {
+  console.log(`My number: ${parsedValue}`);
+} else {
+  console.log('Not a number');
+}
