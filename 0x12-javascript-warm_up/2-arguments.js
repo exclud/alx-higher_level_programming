917af71f4d806481e6ffd8a58d32e629feb14538
exclud <shammahnjomo12@gmail.com>
2023-09-11 23:08:19 +0300

    a script that prints a message depending of the number of arguments passed:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv.slice(2); // The slice is to exclude the node and the script filename.
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
