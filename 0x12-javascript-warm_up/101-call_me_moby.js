2a49f5296ac28e12d10aef99638b17fc8a74a752
exclud <shammahnjomo12@gmail.com>
2023-09-11 23:39:34 +0300

    a function that executes x times a function.

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Define the executeXTimes function
+function executeXTimes (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+// Export the executeXTimes function to make it visible from outside
+module.exports = executeXTimes;
