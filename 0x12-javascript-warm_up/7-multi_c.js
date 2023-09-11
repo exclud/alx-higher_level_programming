244d97240c0327d7a559e8cd43a2414b662f6f3b
exclud <shammahnjomo12@gmail.com>
2023-09-11 23:29:03 +0300

    a script that prints a square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// Get the first argument passed to the script
+const arg1 = process.argv[2];
+
+// Use parseInt to attempt conversion to an integer
+const x = parseInt(arg1);
+
+// Check if x is a valid integer
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+// Get the first argument passed to the script
+const arg1 = process.argv[2];
+
+// Use parseInt to attempt conversion to an integer
+const size = parseInt(arg1);
+
+// Check if size is a valid integer
+if (!isNaN(size)) {
+  // Loop to print each row of the square
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log("Missing size");
+}
