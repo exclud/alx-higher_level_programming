c46d3ed8ba525abf6674802e6f702806bc88bf06
exclud <shammahnjomo12@gmail.com>
2023-09-12 12:09:37 +0300

    Fixed the print function in the rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
-      this.width = w;
-      this.height = h;
-    } else {
-      this.width = undefined;
-      this.height = undefined;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,11 @@
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+      this.print();
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
     }
   }
 };
