07d75c03c5374058dcbac7996be98ccbe4a6759e
exclud <shammahnjomo12@gmail.com>
2023-09-12 12:12:25 +0300

    Semistandard fix in the square class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
-      }
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
 };
