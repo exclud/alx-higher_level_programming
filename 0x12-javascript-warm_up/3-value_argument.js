9e99e62c771aa971cafd375eeb4db12d6496d02a
exclud <shammahnjomo12@gmail.com>
2023-09-12 12:20:44 +0300

    Correction Fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const firstArgument = process.argv.slice[2];
-
-// Check to see if the First Argument is undefined
-if (firstArgument === undefined) {
-  console.log('No argument');
-} else {
-  console.log(firstArgument);
-}
+const { argv } = require('process');
+if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
