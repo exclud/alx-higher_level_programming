3166932d749971237985a097461507a37db51d2e
exclud <shammahnjomo12@gmail.com>
2023-09-12 11:32:05 +0300

    Instance method charPrint(c)

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,45 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+
+  print () {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
+        }
+        console.log(row);
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width !== undefined && this.height !== undefined) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  double () {
+    if (this.width !== undefined && this.height !== undefined) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+class Square extends Rectangle {
+    constructor (size) {
+      super(size, size);
+    }
+
+    charPrint(c) {
+        if (c === undefined) {
+            c = 'X'
+        }
+
+        for (let i = 0; i < this.size; i++){
+            console.log(c.repeat(this.size));
+
+        }
+    }
+  }
+
+class Square extends Square {
+  constructor(size) {
+    super(size);
+  }
+}
