482afe553c4baaa4aa3e597be21d06aa80d3562d
exclud <shammahnjomo12@gmail.com>
2023-09-13 10:10:19 +0300

    a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+// Initialize an empty dictionary to store user ids by occurrence
+const usersByOccurrence = {};
+
+// Iterate through the keys (occurrences) in the initial dictionary
+for (const occurrence in dict) {
+  const userId = dict[occurrence];
+
+  // Check if the occurrence count is already a key in usersByOccurrence
+  if (usersByOccurrence[occurrence]) {
+    // If it is, push the user id to the existing array
+    usersByOccurrence[occurrence].push(userId);
+  } else {
+    // If it is not, create a new array with the user id as the first element
+    usersByOccurrence[occurrence] = [userId];
+  }
+}
+
+console.log(usersByOccurrence);
