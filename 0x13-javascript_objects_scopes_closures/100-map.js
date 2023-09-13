7d17b6d6fdb68d29f7a82b1bcc5a2289a1502c4a
exclud <shammahnjomo12@gmail.com>
2023-09-13 10:05:05 +0300

    Wrote a script that imports an array and computes a new array.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const { list } = require('./100-data');
+
+const newList = list.map((value, index) => value * index);
+
+console.log('Initial List:', list);
+console.log('New List:', newList);
