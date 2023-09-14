ddcca621c44b85c867a15d9d74cbfd2d1c032de3
exclud <shammahnjomo12@gmail.com>
2023-09-14 10:49:18 +0300

    Fixed the semistandard error on map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-let newList = list.map((value, index) => value * index);
+const newList = list.map((value, index) => value * index);
 
 console.log(list);
 console.log(newList);
