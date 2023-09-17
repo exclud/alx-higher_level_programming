2c16b55a880b08007dc30e998aa22cafc980aa79
exclud <shammahnjomo12@gmail.com>
2023-09-17 22:13:20 +0300

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
+from unicodedata import name
 """Function  """
 
 
-def list_states():
-    """
-    Function to list all states from the database hbtn_0e_0_usa
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+# def list_states():
+#     """
+#     Function to list all states from the database hbtn_0e_0_usa
+#     """
+#     db = MySQLdb.connect(host="localhost", port=3306,
+#  user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])#     cur = db.cursor()
+#     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+#     rows = cur.fetchall()
+#     for row in rows:
+#         print(row)
+#     cur.close()
+#     db.close()
 
 if __name__ == "__main__":
     """
     Main function to prevent execution when imported
     """
-    list_states()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
