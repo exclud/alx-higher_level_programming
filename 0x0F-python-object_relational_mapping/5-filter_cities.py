e9596336fa5a5a856096b4ec58b04b5012eb11f9
exclud <shammahnjomo12@gmail.com>
2023-09-17 22:16:45 +0300

    lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,23 +2,33 @@
 import MySQLdb
 import sys
 
-def list_cities():
-    """
-    Function to list all cities from a state in the database hbtn_0e_4_usa
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                 JOIN states ON cities.state_id = states.id \
-                 WHERE states.name=%s ORDER BY cities.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+# def list_cities():
+#     """
+#     Function to list all cities from a state in the database hbtn_0e_4_usa
+#     """
+#     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+# passwd=sys.argv[2], db=sys.argv[3])
+#     cur = db.cursor()
+#     cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+#                  JOIN states ON cities.state_id = states.id \
+#                  WHERE states.name=%s ORDER BY cities.id ASC",
+# (sys.argv[4],))
+#     rows = cur.fetchall()
+#     for row in rows:
+#         print(row)
+#     cur.close()
+#     db.close()
 
 if __name__ == "__main__":
     """
     Main function to prevent execution when imported
     """
-    list_cities()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM cities
+                INNER JOIN states
+                ON cities.state_id = states.id
+                ORDER BY cities.id""")
+    print(", ".join([city[2]
+                     for city in c.fetchall()
+                     if city[4] == sys.argv[4]]))
