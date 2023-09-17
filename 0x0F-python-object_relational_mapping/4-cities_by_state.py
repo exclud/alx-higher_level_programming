c8c6f558d25d9ec00ea134ec64e2ed0ba68d7896
exclud <shammahnjomo12@gmail.com>
2023-09-17 12:09:44 -0700

    executed  a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""
+lists all cities from the database hbtn_0e_4_usa
+"""
 
-def list_cities():
-    """
-    Function to list all cities from the database hbtn_0e_4_usa
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """
-    Main function to prevent execution when imported
-    """
-    list_cities()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states
+                ON cities.state_id = states.id
+                ORDER BY cities.id ASC""")
+    [print(city) for city in c.fetchall()]
