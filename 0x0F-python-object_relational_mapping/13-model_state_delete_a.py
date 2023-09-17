c5fa5bb4e2bb7cb444e1c4cd12e1993b73d64900
exclud <shammahnjomo12@gmail.com>
2023-09-17 21:48:13 +0300

    script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,26 +7,38 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def delete_states_with_a(username, password, db_name):
-    """
-    Function to delete all State objects with a name containing the letter a
-    """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+# def delete_states_with_a(username, password, db_name):
+#     """
+#     Function to delete all State objects with a name containing the letter a
+#     """
+#     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+#                            .format(username, password, db_name), pool_pre_ping=True)
+#     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
 
-    for state in session.query(State).filter(State.name.contains('a')):
-        session.delete(state)
-    session.commit()
+#     for state in session.query(State).filter(State.name.contains('a')):
+#         session.delete(state)
+#     session.commit()
+
+# if __name__ == "__main__":
+#     """
+#     Main function to prevent execution when imported
+#     """
+#     username = sys.argv[1]
+#     password = sys.argv[2]
+#     db_name = sys.argv[3]
+#     delete_states_with_a(username, password, db_name)
 
 if __name__ == "__main__":
-    """
-    Main function to prevent execution when imported
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    delete_states_with_a(username, password, db_name)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        session.delete(state)
+
+    session.commit()
