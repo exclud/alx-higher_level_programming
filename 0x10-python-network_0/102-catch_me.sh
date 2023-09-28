66bcc24a302f2a600f7c20b83020c4419f3f1b16
exclud <shammahnjomo12@gmail.com>
2023-09-28 20:24:37 +0300

    script sends a request to 0.0.0.0:5000/catch_me to get the server response without displaying it directly.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me to get the server response without displaying it directly.
-curl -sL -X PUT -H -d "user_id=98" 0.0.0.0:5000/catch_me_ ifconfig | awk '/inet /{print $2}'
+curl -sL -X PUT -H "Origin: PleaseWork" -d "user_id=98" 0.0.0.0:5000/catch_me
