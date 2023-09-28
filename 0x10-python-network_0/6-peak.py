76c6ac65513ff2496d25462d6ce4c4ed64a5bb7f
exclud <shammahnjomo12@gmail.com>
2023-09-28 20:12:36 +0300

    Technical Interview Solution

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,30 @@
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+    list_of_integers (list): List of unsorted integers.
+
+    Returns:
+    int: A peak value from the list.
+
+    Complexity:
+    The complexity of this algorithm is O(log(n)), as it
+    divides the list in half with each iteration.
+    """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # If mid element is greater than its right neighbor, move left.
+            high = mid
+        else:
+            # Otherwise, move right.
+            low = mid + 1
+
+    return list_of_integers[low]
