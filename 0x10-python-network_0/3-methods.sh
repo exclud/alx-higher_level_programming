ac1a701b34e17a2ccfd2a19434ac2cead7c10b94
exclud <shammahnjomo12@gmail.com>
2023-09-28 20:20:48 +0300

    URL Script Fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument and displays all HTTP methods the server will accept.
-curl -sI --head "$1" | grep -i 'Allow' | awk '{print $2}'
+curl -sI --head "$1" | grep -i 'Allow' | cut -d " " -f 2-
