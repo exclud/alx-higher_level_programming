58f34d4cd3e122385dd28fddc82d566d92e0e29f
exclud <shammahnjomo12@gmail.com>
2023-09-28 20:22:34 +0300

    GET request script fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request, and displays the body of the response for a 200 status code.
-[ "$(curl -s -w '%{http_code}' "$1")" -eq 200 ] && curl -s "$1"
+curl -sL "$1"
