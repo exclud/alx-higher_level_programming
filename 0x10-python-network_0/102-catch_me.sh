5d431478a713eb8cb5d98c3a9fd1368cee809e65
exclud <shammahnjomo12@gmail.com>
2023-09-28 20:34:38 +0300

    Last Resort

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me to get the server response without displaying it directly.
-curl -sL -X PUT -H "Origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
