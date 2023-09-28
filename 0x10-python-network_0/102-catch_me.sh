b3fceab0e49d1aa4b20f6d1ff9c25153f04cca96
exclud <shammahnjomo12@gmail.com>
2023-09-28 20:19:09 +0300

    Script Fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me to get the server response without displaying it directly.
-curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me_ ifconfig | awk '/inet /{print $2}'
+curl -sL -X PUT -H -d "user_id=98" 0.0.0.0:5000/catch_me_ ifconfig | awk '/inet /{print $2}'
