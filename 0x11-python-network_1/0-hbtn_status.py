23f9d10037e4f0e796171e0616a46fccff35e6e9
exclud <shammahnjomo12@gmail.com>
2023-09-29 15:10:19 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script fetches and displays the body of a URL using urllib.
+"""
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        decoded_content = content.decode('utf-8')
+
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(decoded_content))
