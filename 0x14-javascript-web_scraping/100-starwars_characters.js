b090af1e768880e0ed71783f227deb8a55f9d08c
exclud <shammahnjomo12@gmail.com>
2023-10-24 22:20:30 +0300

    Web Scrapping Using Javascript node js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,51 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function fetchCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) return reject(error);
+      if (response.statusCode !== 200) return reject('Failed to retrieve character');
+
+      const character = JSON.parse(body);
+      resolve(character.name);
+    });
+  });
+}
+
+function fetchMovieCharacters (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+
+  request(url, async (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error('Unexpected status code:', response.statusCode);
+      return;
+    }
+
+    const film = JSON.parse(body);
+    const characterUrls = film.characters;
+
+    for (const url of characterUrls) {
+      try {
+        const name = await fetchCharacterName(url);
+        console.log(name);
+      } catch (error) {
+        console.error('Error fetching character name:', error);
+      }
+    }
+  });
+}
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: node getCharacters.js <Movie ID>');
+  process.exit(1);
+}
+
+fetchMovieCharacters(movieId);
---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node getCharacters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+
+request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characterUrls = film.characters;
+
+  characterUrls.forEach((characterUrl, index) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('Unexpected status code:', response.statusCode);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
